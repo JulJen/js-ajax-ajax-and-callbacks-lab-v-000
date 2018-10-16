5922993e727ab195910f1d5d65eabc4fa3cf3a1b
Jul Jen <juliajen.prog@gmail.com>
2018-10-16 02:12:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,49 +6,41 @@ function displayError(error) {
 // }
 
 
+function searchRepositories() {
+  const input = document.getElementById("searchTerms").value
+  const url = `https://api.github.com/search/repositories?q=${input}`
+
+
+
+    $.get(url, function(data) {
+      console.log(data.items);
+
+    const repoList = `<ul>${data.items
+    .map(r => '<li>' + r.name + ', ' + r.html_url + '</li>' + `<a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a>`)
+    .join('')}</ul>`;
+    document.getElementById("results").innerHTML = repoList;
+    }).fail(displayError())
+
+
+}
+
+
+function displayError() {
+  document.getElementById("errors").innerHTML = "I'm sorry, there's been an error. Please try again.";
+}
+
 function showCommits(el) {
   const repo = el.dataset.repository;
   const user = el.dataset.owner;
   const url = `https://api.github.com/repos/${user}/${repo}/commits`
-
   $.get(url, function(data){
-    const results = `<ul>${data
+    const commitList = `<ul>${data
     .map(r => '<li>' + r.commit.author.name + ', ' + r.sha + '</li>')
     .join('')}</ul>`;
-    document.getElementById("details").innerHTML = results;
+    document.getElementById("details").innerHTML = commitList;
   })
-
+ 
 }
-
-
-function showRepositories(result)  {
-  const repos = result.items;
-  const repoList = `<ul>${repos
-       .map(
-         repo =>
-           '<li>' +
-           '<strong>' +  repo.name + '</strong> -' +
-           repo.description + '<br>' +
-           '<a href="' + repo.html_url + '">' + repo.html_url + '</a>' + '<br>' +
-           '<a href="#" onclick="showCommits(this)" data-repository="' + repo.name + '" data-owner="' + repo.owner.login + '">Show Commits</a>' +
-           '</li>'
-       )
-       .join('')}</ul>`;
-
-  document.getElementById("results").innerHTML = repoList;
-}
-
-
-function searchRepositories() {
-  const searchTerms = $("#searchTerms").val();
-  const url = "https://api.github.com/search/repositories?q=";
-
-  $.get(`${url}${searchTerms}`)
-    .done(showRepositories)
-    .fail(displayError);
-}
-
-
 
 
 $(document).ready(function() {
